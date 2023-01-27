b283d24dfe92a3217fed03a1356f7e763c73813a
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2023-01-26 19:04:24 -0800

    Update Source.lua

--- ESP/Source.lua
+++ ESP/Source.lua
@@ -1,1 +1,41 @@
-
+if not game:GetService("CoreGui"):FindFirstChild("Chams") then
+    local Chams = Instance.new("Folder")
+    Chams.Parent = game:GetService("CoreGui")
+    Chams.Name = "Chams"
+end
+local Utility = {}
+function Utility.AddPlayer(name, args)
+  local cfg = args or {}
+	local Enabled = cfg.Enabled or true
+	local Color1 = cfg.FColor or Color3.fromRGB(255, 0, 0)
+	local Color2 = cfg.OColor or Color3.fromRGB(0, 0, 0)
+	local Transparency1 = cfg.FTransparency or 0
+	local Transparency2 = cfg.OTransparency or 0
+	local Depth = cfg.Depth or "AlwaysOnTop"
+	local Folder = cfg.Folder or Chams
+	local Highlight = Instance.new("Highlight"):Clone()
+	repeat task.wait() until game.Players[name]:FindFirstChild("Character")
+	Highlight.Adornee = game.Players[name].Character
+	Highlight.Enabled = Enabled
+	Highlight.Parent = f
+	Highlight.Name = name
+	Highlight.FillColor = Color1
+	Highlight.OutlineColor = Color2
+	Highlight.FillTransparency = Transparency1
+	Highlight.OutlineTransparency = Transparency2
+	Highlight.DepthMode = Depth
+end
+function Utility.Team(plr)
+    if plr.Team == game:GetService("Players").LocalPlayer.Team then
+		return true else return false end
+end
+function Utility.Health(plr)
+    repeat task.wait() until plr.Character
+	local Humanoid = plr.Character:FindFirstChildOfClass("Humanoid")
+	if Humanoid then return Humanoid.Health end
+end
+function Utility.Unload()
+    for _, v in next, Chams:GetChildren() do
+		v:Destroy()
+	end
+end
